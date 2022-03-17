49844b844b0ca43a7ab4a4c5bab75ccb9d6e8f23
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-03-17 18:35:57 -0300

    upload lista

---
+++ lista .py
@@ -0,0 +1,11 @@
+lista = [5,3,5.5,'Jhonni',True]
+lista2 = ['Proway',lista]
+print(lista2)
+# print(type(lista2))
+print(lista2[0])
+# print(type(lista2[0]))
+print(type(lista2[0]))
+if type(lista2[0]) is str:
+    print(lista2[0] + ' é uma String')
+# print(lista2[1])
+# print(type(lista2[1]))
